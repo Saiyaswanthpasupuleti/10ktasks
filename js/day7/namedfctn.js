fd31188f1e82b2dd56f02d27ff5aef130eb8b995
Macbook <macbook@Macbooks-MacBook-Air.local>
2024-08-12 20:05:33 +0530

    s

---
+++ js/day7/namedfctn.js
@@ -0,0 +1,170 @@
+// function sai(a){
+   
+
+//     for(let i=1;i<=a;i++){
+//        var sum=i**2;
+//            }
+//            console.log(sum)
+//            return "completed"
+    
+// }
+// sai(2)
+
+
+// function sai(a){
+    
+//     for(let i=1;i<=a;i++){
+//         var b=i**3
+//     }
+//     console.log(b)
+// }
+// sai(3)
+
+
+// function sai(a){
+//     let sum=0;
+    
+//     for(let i=1;i<=a;i++){
+
+//         if(i%3==0){
+          
+//            sum=sum+i
+           
+            
+//         }
+//     }
+//     console.log(sum)
+// }
+// sai(20)
+
+
+
+// function sai(a){
+// for(let i=a;i>=1;i--){
+//     console.log(i)
+// }
+// }
+// sai(10)
+
+// function sai(a){
+//     let sum=1;
+//     for(let i=1;i<=a;i++){
+//         sum=sum*i
+
+//     }
+//     console.log(sum)
+// }
+// sai(12)
+
+// function sai(a){
+//     for(let i=a;i>=1;i--){
+//         if(i%2==0){
+//             console.log(i)
+//         }
+//     }
+// }
+// sai(20)
+
+
+// let a=(a)=>{
+//     for(let i=1;i<=a;i++){
+//         if(i%5!==0){
+            
+//             console.log(i)
+            
+//         }
+        
+//     }
+//     return "end"
+// }
+// console.log(a(20))
+
+// let a=(b)=>{
+//     var sum=0;
+//     for(let i=1;i<=b;i++){
+//         if(i%4==0){
+            
+//             sum=sum+i;
+            
+//         }
+//     }
+//     console.log(sum)
+//     return "end"
+// }
+// console.log(a(30))
+
+
+
+// sai=(a)=>{
+//     var c=0;
+// for(let i=a;i>=1;i-- ){
+//     if(i%3==0){
+//         c=c+i
+//     }
+// }
+// console.log(c)
+// return "end"
+// }
+// console.log(sai(50))
+
+
+// let a=(a)=>{
+//     for(let i=1;i<=a;i++){
+//         if(i%2==0){
+//             console.log(i)
+//         }
+//     }
+//     return "end"
+// }
+// console.log(a(100))
+
+
+
+// let a=(a)=>{
+//     let b=a.toString();
+//     let sum;
+//     var c=1;
+//     for(let i=0;i<b.length;i++){
+//         sum=b[i];
+//         c=c*sum
+        
+//     }
+
+//     console.log(c)
+//     return "end"
+
+// }
+// console.log(a(3431));
+
+// let a=(a)=>{
+//     var even=0;
+//     var e;
+//     var o;
+//     let odd=0;
+//     let o1;
+//     for(let i=1;i<=a;i++){
+        
+//         if(true){
+//             if(i%2===0){
+//           e=i;
+//           even=even+i
+//             }
+//           if(i%3===0){
+//             o=i;
+//             odd=odd+i
+            
+//           }
+//         }
+        
+//     }
+//     console.log(even,odd)
+//     if(even < odd){
+//         console.log("even is lesser")
+//       }
+//       else {
+//         console.log("odd is lesser")
+//       }
+//       return "end"
+    
+// }
+// console.log(a(100));
