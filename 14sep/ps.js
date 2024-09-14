3d6be207ee5cac52ad9ce9d772d2024ba5a6de6c
Saiyaswanthpasupuleti <saiyaswanths959@gmail.com>
2024-09-14 11:43:49 +0530

    new

---
+++ 14sep/ps.js
@@ -0,0 +1,37 @@
+// let rows=prompt("enter number of rows : ")
+// for(let i=rows;i>=1;i--){
+//     res="";
+//     for(let k=rows-i;k>=1;k--){
+//         res+=" "
+//     }
+    
+//     for(let j=i;j>=1;j--){
+//         if(i==rows || j==1 || i-j==j-i){
+//         res+="*"+" "
+//         }
+//         else{
+//             res+="  "
+//         }
+//     }
+//     console.log(res)
+// }
+
+
+
+
+
+
+// let rows=prompt("enter number of rows : ")
+// count=1
+// for(let i=1;i<=rows;i++){
+//     res="";
+//     for(let j=1;j<=i;j++){
+//         res+=count+" "
+//         count++
+//     }
+//  var o=res.split(" ")
+//  var p=o.reverse().join(" ");
+//  console.log(p)
+ 
+// }
+
