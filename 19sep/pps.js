6d2d9d1dc59345c12b8c9f7cd8f175f65cdc931c
Saiyaswanthpasupuleti <saiyaswanths959@gmail.com>
2024-09-20 12:24:49 +0530

    new

---
+++ 19sep/pps.js
@@ -0,0 +1,14 @@
+// let rows=5;
+// for(let i=1;i<=2*rows-1;i++){
+//     cols=(i<=rows)?i : 2*rows-i;
+//     space=(i<=rows)?rows-i:i-rows;
+//     res="";
+//     for(let k=1;k<=space;k++){
+//         res+=" "
+//     }
+//     for(let j=1;j<=cols;j++){
+//         res+=j+" "
+//     }
+//     console.log(res)
+// }
+
