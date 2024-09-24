6ba4caef61a9919c4aa3eac27e632ebff8c5b21b
Saiyaswanthpasupuleti <saiyaswanths959@gmail.com>
2024-09-24 20:31:34 +0530

    new

--- 23sep/ps.js
+++ 23sep/ps.js
@@ -19,20 +19,62 @@
 //     }
 // }
 
-let rows=5;
-char=65;
-for(let i=1;i<=rows;i++){
-    res=""
-    space=""
-    for(let k=1;k<=rows-i;k++){
-        res+=" "
-    }
-    let temp=""
-    for(let j=1;j<=i;j++){
+// let rows=5;
+// char=65;
+// for(let i=1;i<=rows;i++){
+//     res=""
+//     space=""
+//     for(let k=1;k<=rows-i;k++){
+//         res+=" "
+//     }
+//     let temp=""
+//     for(let j=1;j<=i;j++){
         
-        temp+=String.fromCharCode(char)
-        char++
-    }
-    res+=temp.split("").reverse().join("")
-    console.log(res)
-}
+//         temp+=String.fromCharCode(char)
+//         char++
+//     }
+//     res+=temp.split("").reverse().join("")
+//     console.log(res)
+// }
+
+
+
+// let rows=7;
+// for(let i=0;i<rows;i++){
+//     res=""
+//     char=65
+//     space=""
+//     for(let k=0;k<=2*i-1;k++){
+//         space+=" "
+//     }
+//     for(let j=1;j<=rows-i;j++){
+//         res+=String.fromCharCode(char)
+//         char++
+//     }
+//     console.log(res+space+res)
+
+// }
+
+// let rows=7
+// for(let i=1;i<=rows;i++){
+//     res=""
+//     space=""
+//     for(let k=1;k<=2*(rows-i);k++){
+//         space+=" "
+//     }
+//     for(let j=1;j<=i;j++){
+//         res+="*"
+//     }
+//     console.log(res+space+res)
+// }
+// for(let i=rows-1;i>=1;i--){
+//     res=""
+//     space=""
+//     for(let k=1;k<=2*(rows-i);k++){
+//         space+=" "
+//     }
+//     for(let j=i;j>=1;j--){
+//         res+="*"
+//     }
+//     console.log(res+space+res)
+// }
--- 25sep/ps.js
+++ 25sep/ps.js
@@ -1,12 +1,83 @@
+
+
+// let rows=7;
+// for(let i=0;i<rows;i++){
+//     res=""
+//     char=65
+//     space=""
+//     for(let k=0;k<=2*i-1;k++){
+//         space+=" "
+//     }
+//     for(let j=1;j<=rows-i;j++){
+//         res+=String.fromCharCode(char)
+//         char++
+//     }
+//     console.log(res+space+res)
+
+// }
+
+// let rows=7
+// for(let i=1;i<=rows;i++){
+//     res=""
+//     space=""
+//     for(let k=1;k<=2*(rows-i);k++){
+//         space+=" "
+//     }
+//     for(let j=1;j<=i;j++){
+//         res+="*"
+//     }
+//     console.log(res+space+res)
+// }
+// for(let i=rows-1;i>=1;i--){
+//     res=""
+//     space=""
+//     for(let k=1;k<=2*(rows-i);k++){
+//         space+=" "
+//     }
+//     for(let j=i;j>=1;j--){
+//         res+="*"
+//     }
+//     console.log(res+space+res)
+// }
+
+
+
+
+
+
 let rows=7;
 for(let i=1;i<=rows;i++){
     res=""
     space=""
-    for(let k=1;k<=2*i-1;k++){
+    for(let k=1;k<=2*(rows-i);k++){
         space+=" "
     }
-    for(let j=1;j<=rows-i;j++){
-        res+="*"
+    for(let j=1;j<=i;j++){
+        if(j%2!==0){
+            res+="1"
+        }
+        else{
+            res+="0"
+        }
+        
+        
+    }
+    console.log(res+space+res)
+}
+for(let i=rows-1;i>=1;i--){
+    res=""
+    space=""
+    for(let k=1;k<=2*(rows-i);k++){
+        space+=" "
+    }
+    for(let j=1;j<=i;j++){
+        if(j%2!==0){
+            res+="1"
+        }
+        else{
+            res+="0"
+        }
+        
     }
     console.log(res+space+res)
 }
