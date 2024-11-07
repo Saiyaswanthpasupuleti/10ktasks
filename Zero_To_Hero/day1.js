ae7de515f15abd6de912bee5e9a3f9a4b30ad63f
Saiyaswanthpasupuleti <saiyaswanths959@gmail.com>
2024-11-07 17:50:18 +0530

    new

--- Zero_To_Hero/day1.js
+++ Zero_To_Hero/day1.js
@@ -17,7 +17,7 @@
 //Or u can use the formula n*(n+1)/2
 
 
-
+//----------------------------------------------------------------
 
 //Sum of natural numbers : 
 
@@ -31,3 +31,29 @@
 
 // }
 // console.log(sum(123))
+//----------------------------------------------------------------
+
+//No of digits 
+
+
+// function a(n) {
+//     let x = Math.abs(n); // Get the absolute value of n
+//     let count = 0;
+//     do {
+//         count++; // Increment the count
+//         x = Math.floor(x / 10); // Reduce x by one digit
+//     } while (x > 0); // Continue while x is greater than 0
+//     return count; // Return the number of digits
+// }
+
+// console.log(a(-34252)); // Outputs the number of digits in 34251
+
+// let num=123
+// let cout=0;
+// let x=num.toString().split("")
+// for(let i of x){
+//     if(!isNaN(Number(i))){
+//         cout++
+//     }
+// }
+// console.log(cout)
