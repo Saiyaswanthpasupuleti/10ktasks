5d9fae657ca78530a5eadda56e92ba0cbd84036a
Saiyaswanthpasupuleti <saiyaswanths959@gmail.com>
2024-11-07 10:58:09 +0530

    new

---
+++ Zero_To_Hero/day1.js
@@ -0,0 +1,33 @@
+//Sum of all Natural Numbers :
+
+
+// function sum(n){
+//     let res=0;
+//     for(let i=1;i<=n;i++){
+//         res=res+i
+//     }
+//     return res;
+
+// }
+// console.log(sum(10))
+
+
+
+
+//Or u can use the formula n*(n+1)/2
+
+
+
+
+//Sum of natural numbers : 
+
+// function sum(n){
+//     let y=0;
+//     let x=n.toString().split("")
+//     for(let i of x){
+//         y+=Number(i)
+//     }
+//     return y
+
+// }
+// console.log(sum(123))
