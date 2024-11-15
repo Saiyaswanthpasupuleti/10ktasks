89a79126410e74953d2ab736dcc436a1f6c8f399
Saiyaswanthpasupuleti <saiyaswanths959@gmail.com>
2024-11-15 12:05:28 +0530

    new

--- Node/Day6/task.js
+++ Node/Day6/task.js
@@ -1,86 +1,86 @@
 
 // Task 1    
 
-let http=require("http");
-let fs=require("fs")
-let server=http.createServer((req,res)=>{
-    if(req.method=="GET"){
-        let msg="This is Get Method"
-        fs.writeFile("./postmethod.txt",msg,(err,data)=>{
-            if(err){
-                console.log(err)
-            }
-            else{
-                fs.readFile("./postmethod.txt","utf-8",(err,data)=>{
-                    if(err){
-                        console.log(err)
-                    }
-                    else{
-                        res.write(data)
-                        console.log(data)
-                    }
-                })
-            }
-        })
-    }
-    else if(req.method=="POST"){
-        let msg="This is Post Method";
-        fs.appendFile("./postmethod.txt",msg,(err,data)=>{
-            if(err){
-                console.log(err)
-            }
-            else{
-                fs.readFile("./postmethod.txt","utf-8",(err,data)=>{
-                    if(err){
-                        console.log(err)
-                    }
-                    else{
-                        res.write(data)
-                        console.log(data)
-                    }
-                })
-            }
+// let http=require("http");
+// let fs=require("fs")
+// let server=http.createServer((req,res)=>{
+//     if(req.method=="GET"){
+//         let msg="This is Get Method"
+//         fs.writeFile("./postmethod.txt",msg,(err,data)=>{
+//             if(err){
+//                 console.log(err)
+//             }
+//             else{
+//                 fs.readFile("./postmethod.txt","utf-8",(err,data)=>{
+//                     if(err){
+//                         console.log(err)
+//                     }
+//                     else{
+//                         res.write(data)
+//                         console.log(data)
+//                     }
+//                 })
+//             }
+//         })
+//     }
+//     else if(req.method=="POST"){
+//         let msg="This is Post Method";
+//         fs.appendFile("./postmethod.txt",msg,(err,data)=>{
+//             if(err){
+//                 console.log(err)
+//             }
+//             else{
+//                 fs.readFile("./postmethod.txt","utf-8",(err,data)=>{
+//                     if(err){
+//                         console.log(err)
+//                     }
+//                     else{
+//                         res.write(data)
+//                         console.log(data)
+//                     }
+//                 })
+//             }
 
-        })
-    }
+//         })
+//     }
 
-    else if(req.method=="PUT"){
-        let msg="This is Put Method";
-        fs.writeFile("./postmethod.txt",msg,(err,data)=>{
-            if(err){
-                console.log(err)
-            }
-            else{
-                fs.readFile("./postmethod.txt","utf-8",(err,data)=>{
-                    if(err){
-                        console.log(err)
-                    }
-                    else{
-                        res.write(data)
-                        console.log(data)
-                    }
-                })
-            }
+//     else if(req.method=="PUT"){
+//         let msg="This is Put Method";
+//         fs.writeFile("./postmethod.txt",msg,(err,data)=>{
+//             if(err){
+//                 console.log(err)
+//             }
+//             else{
+//                 fs.readFile("./postmethod.txt","utf-8",(err,data)=>{
+//                     if(err){
+//                         console.log(err)
+//                     }
+//                     else{
+//                         res.write(data)
+//                         console.log(data)
+//                     }
+//                 })
+//             }
 
-        })
-    }
-    else if(req.method=="DELETE"){
-        fs.unlink("./postmethod.txt",(err,data)=>{
-            if(err){
-                console.log(err)
-            }
-            else{
-                console.log("Succesfully deleted the file")
-            }
-        })
-    }
+//         })
+//     }
+//     else if(req.method=="DELETE"){
+//         fs.unlink("./postmethod.txt",(err,data)=>{
+//             if(err){
+//                 console.log(err)
+//             }
+//             else{
+//                 console.log("Succesfully deleted the file")
+//             }
+//         })
+//     }
 
     
     
-})
-server.listen(3000,()=>{
-    console.log("Can Can !_!")
-})
+// })
+// server.listen(3000,()=>{
+//     console.log("Can Can !_!")
+// })
 
 
 
@@ -95,36 +95,48 @@ server.listen(3000,()=>{
 
 
 
-// let http=require("http");
-// let server=http.createServer(async (req,res)=>{
-//     let a=await fetch("https://fakestoreapi.com/products")
-//     let b=await a.json()
+let http=require("http");
+let server=http.createServer(async (req,res)=>{
+    let a=await fetch("https://fakestoreapi.com/products")
+    let b=await a.json()
     
-//     if(req.method==="GET"){
-//         console.log("Get",b[0])
-//         res.write(JSON.stringify(b[0]))
-//     }
-//     else if(req.method==="POST"){
-//         console.log("POST",b[1])
-//         res.write(JSON.stringify(b[1]))
+    if(req.method==="GET"){
+        let id=0
+        let x=b.filter((val,index,array)=>{
+            return val.id==id
+        })
+        console.log(x[0])
+    }
+    else if(req.method==="POST"){
+        let id=1
+        let x=b.filter((val,index,array)=>{
+            return val.id==id
+        })
+        console.log(x[0])
 
-//     }
-//     else if(req.method==="PUT"){
-//         console.log("PUT",b[2])
-//         res.write(JSON.stringify(b[2]))
+    }
+    else if(req.method==="PUT"){
+        let id=2
+        let x=b.filter((val,index,array)=>{
+            return val.id==id
+        })
+        console.log(x[0])
 
-//     }
-//     else if(req.method==="DELETE"){
-//         console.log("DELETE",b[3])
-//         res.write(JSON.stringify(b[3]))
+    }
+    else if(req.method==="DELETE"){
+        let id=3
+        let x=b.filter((val,index,array)=>{
+            return val.id==id
+        })
+        console.log(x[0])
 
-//     }
-//     else{
-//         res.end(JSON.stringify("Server Stopped"))
-//         console.log("Ended")
-//     }
+    }
+    else{
+        res.end(JSON.stringify("Server Stopped"))
+        console.log("Ended")
+    }
 
-// })
-// server.listen(3000,()=>{
-//     console.log("Can Can !_!")
-// })
+})
+server.listen(3000,()=>{
+    console.log("Can Can !_!")
+})
